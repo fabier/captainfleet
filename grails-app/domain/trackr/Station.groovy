cc78050594a93f1fcd8248d9e33bbdf9ca9ef1a9
Pierre FABIER <fabier@free.fr>
2015-05-20 17:40:25 +0200

    Ajout de la position géographique d'une station dans le modèle de données

--- grails-app/domain/trackr/Station.groovy
+++ grails-app/domain/trackr/Station.groovy
@@ -1,11 +1,19 @@
 package trackr
 
+import com.vividsolutions.jts.geom.Geometry
+
 class Station extends BaseEntity {
 
     // Identifiant sigfox
     String sigfoxId
 
+    /**
+     * Position de la station (valeur estimée en fonction des différentes qualités de signal)
+     */
+    Geometry position
+
     static constraints = {
         sigfoxId nullable: false, unique: true
+        position nullable: true
     }
 }
