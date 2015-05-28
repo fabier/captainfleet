0cbb4387035aa13357c5333a8f1171944c77c09c
Pierre FABIER <fabier@free.fr>
2015-05-28 09:48:02 +0200

    BUGFIX revert grailsApplication declaration

--- grails-app/controllers/trackr/RegisterController.groovy
+++ grails-app/controllers/trackr/RegisterController.groovy
@@ -8,8 +8,6 @@ import org.springframework.security.access.annotation.Secured
 @Secured("permitAll")
 class RegisterController extends grails.plugin.springsecurity.ui.RegisterController {
 
-    def grailsApplication
-
     def index() {
         def copy = [:] + (flash.chainedParams ?: [:])
         copy.remove 'controller'
@@ -24,7 +22,7 @@ class RegisterController extends grails.plugin.springsecurity.ui.RegisterControl
             return
         }
 
-        def User = grailsApplication.getDomainClass(SpringSecurityUtils.securityConfig.userLookup.userDomainClassName).clazz
+        def User = command.grailsApplication.getDomainClass(SpringSecurityUtils.securityConfig.userLookup.userDomainClassName).clazz
         def user = User.findByEmail(command.email)
         if (user) {
             if (user.accountLocked) {
@@ -131,6 +129,8 @@ class RegisterCommand {
     String password
     String password2
     boolean acceptsConditions
+
+    def grailsApplication
 
     static constraints = {
         username blank: false
