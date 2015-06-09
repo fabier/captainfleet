e231363f7aeac864cdbbbd4401e6c8e94386b83b
Pierre FABIER <fabier@free.fr>
2015-06-09 12:43:19 +0200

    Correction inversion d'affichage des données

--- src/groovy/trackr/FrameData_V2.groovy
+++ src/groovy/trackr/FrameData_V2.groovy
@@ -80,13 +80,13 @@ class FrameData_V2 extends FrameData {
     @Override
     String hexaSolarArrayVoltage() {
         // IIIII : Tension supercapacité entre 0 et 2.91V, pas de 90mV
-        String.format("0b%5s", Integer.toBinaryString((int) (superCapacitorVoltage / 0.09d))).replaceAll(' ', '0')
+        String.format("0b%5s", Integer.toBinaryString((int) (solarArrayVoltage / 0.09d))).replaceAll(' ', '0')
     }
 
     @Override
     String hexaSuperCapacitorVoltage() {
         // HHHHH : Tension panneau solaire entre 0 et 3.1V, pas de 100mV
-        String.format("0b%5s", Integer.toBinaryString((int) (solarArrayVoltage / 0.1d))).replaceAll(' ', '0')
+        String.format("0b%5s", Integer.toBinaryString((int) (superCapacitorVoltage / 0.1d))).replaceAll(' ', '0')
     }
 
     @Override
