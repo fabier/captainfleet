4ab0d8cefcfec8fabb0cce2a67abb97363b201d2
Pierre FABIER <pierre.fabier@gmail.com>
2015-07-17 10:32:40 +0200

    Renommage de la base de données en captainfleet plutot que trackr

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -2,7 +2,7 @@ dataSource {
     pooled = true
     dbCreate = "update"
     driverClassName = "org.postgresql.Driver"
-    url = "jdbc:postgresql://localhost:5432/trackr"
+    url = "jdbc:postgresql://localhost:5432/captainfleet"
     dialect = org.hibernatespatial.postgis.PostgisDialect
     username = "dev"
     password = "dev"
