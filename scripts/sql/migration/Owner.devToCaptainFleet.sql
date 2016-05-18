34eaba71a8e34f6017a1646a4a69d04221dd2bd0
Pierre FABIER <pierre.fabier@gmail.com>
2016-05-18 20:27:44 +0200

    Migration du owner de dev vers captainfleet

---
+++ scripts/sql/migration/Owner.devToCaptainFleet.sql
@@ -0,0 +1,20 @@
+ALTER TABLE public.device OWNER TO captainfleet;
+ALTER TABLE public.device_zone OWNER TO captainfleet;
+ALTER TABLE public.device_zone_log OWNER TO captainfleet;
+ALTER TABLE public.device_zone_log_aggregate OWNER TO captainfleet;
+ALTER TABLE public.device_type OWNER TO captainfleet;
+ALTER TABLE public.frame OWNER TO captainfleet;
+ALTER TABLE public.frame_extra OWNER TO captainfleet;
+ALTER TABLE public.map_marker_icon OWNER TO captainfleet;
+ALTER TABLE public.point OWNER TO captainfleet;
+ALTER TABLE public.registration_code OWNER TO captainfleet;
+ALTER TABLE public."role" OWNER TO captainfleet;
+ALTER TABLE public.spatial_ref_sys OWNER TO captainfleet;
+ALTER TABLE public.station OWNER TO captainfleet;
+ALTER TABLE public."user" OWNER TO captainfleet;
+ALTER TABLE public.user_device OWNER TO captainfleet;
+ALTER TABLE public.user_role OWNER TO captainfleet;
+ALTER TABLE public.user_zone OWNER TO captainfleet;
+ALTER TABLE public."zone" OWNER TO captainfleet;
+
+ALTER TABLE public.hibernate_sequence OWNER TO captainfleet;
